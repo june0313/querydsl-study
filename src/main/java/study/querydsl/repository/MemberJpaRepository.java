8fab670072d0a85e89dbd58e3e83151240154c93
Young Jun Park <june0313@gmail.com>
2020-05-17 10:12:15 +0900

    동적쿼리
     1. BooleanBuilder
     2. Where Parameters

--- src/main/java/study/querydsl/repository/MemberJpaRepository.java
+++ src/main/java/study/querydsl/repository/MemberJpaRepository.java
@@ -1,14 +1,24 @@
 package study.querydsl.repository;
 
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
+import study.querydsl.dto.MemberSearchCondition;
+import study.querydsl.dto.MemberTeamDto;
+import study.querydsl.dto.QMemberTeamDto;
 import study.querydsl.entity.Member;
 
 import javax.persistence.EntityManager;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Function;
 
+import static org.springframework.util.StringUtils.hasText;
 import static study.querydsl.entity.QMember.member;
+import static study.querydsl.entity.QTeam.team;
 
 @Repository
 public class MemberJpaRepository {
@@ -51,5 +61,82 @@ public class MemberJpaRepository {
                 .selectFrom(member)
                 .where(member.username.eq(username))
                 .fetch();
+    }
+
+    public List<MemberTeamDto> searchByBuilder(MemberSearchCondition condition) {
+        BooleanBuilder builder = new BooleanBuilder();
+        if (hasText(condition.getUsername())) {
+            builder.and(member.username.eq(condition.getUsername()));
+        }
+
+        if (hasText(condition.getTeamName())) {
+            builder.and(team.name.eq(condition.getTeamName()));
+        }
+
+        if (condition.getAgeGoe() != null) {
+            builder.and(member.age.goe(condition.getAgeGoe()));
+        }
+
+        if (condition.getAgeLoe() != null) {
+            builder.and(member.age.loe(condition.getAgeLoe()));
+        }
+
+        return queryFactory
+                .select(new QMemberTeamDto(
+                        member.id.as("memberId"),
+                        member.username,
+                        member.age,
+                        team.id.as("teamId"),
+                        team.name.as("teamName")))
+                .from(member)
+                .join(member.team, team)
+                .where(builder)
+                .fetch();
+    }
+
+    public List<MemberTeamDto> search(MemberSearchCondition condition) {
+        return queryFactory
+                .select(new QMemberTeamDto(
+                        member.id.as("memberId"),
+                        member.username,
+                        member.age,
+                        team.id.as("teamId"),
+                        team.name.as("teamName")))
+                .from(member)
+                .join(member.team, team)
+                .where(
+                        nullableExpression(member.username::eq, condition.getUsername()),
+                        nullableExpression(member.team.name::eq, condition.getTeamName()),
+                        nullableExpression(member.age::goe, condition.getAgeGoe()),
+                        nullableExpression(member.age::loe, condition.getAgeLoe())
+                )
+                .fetch();
+    }
+
+    public List<Member> searchMember(MemberSearchCondition condition) {
+        return queryFactory
+                .selectFrom(member)
+                .join(member.team, team)
+                .where(
+                        nullableExpression(member.username::eq, condition.getUsername()),
+                        nullableExpression(member.team.name::eq, condition.getTeamName()),
+                        nullableExpression(member.age::goe, condition.getAgeGoe()),
+                        nullableExpression(member.age::loe, condition.getAgeLoe())
+                )
+                .fetch();
+    }
+
+
+    private BooleanExpression nullableExpression(Function<String, BooleanExpression> function, String value) {
+        return hasText(value) ? function.apply(value) : null;
+    }
+
+    private BooleanExpression nullableExpression(Function<Integer, BooleanExpression> function, Integer value) {
+        return value != null ? function.apply(value) : null;
+    }
+
+    private BooleanExpression nullableExpression(Function<String[], BooleanExpression> function, String ...value) {
+        String[] values = Arrays.stream(value).filter(StringUtils::hasText).toArray(String[]::new);
+        return values.length != 0 ? function.apply(value) : null;
     }
 }
