965239e6ad8f250652fa129c79edbc8a36f29bba
Young Jun Park <june0313@gmail.com>
2020-05-21 21:04:18 +0900

    스프링 데이터 페이징 활용
    - QueryDSL 페이징 연동

--- src/main/java/study/querydsl/repository/MemberRepositoryImpl.java
+++ src/main/java/study/querydsl/repository/MemberRepositoryImpl.java
@@ -1,8 +1,11 @@
 package study.querydsl.repository;
 
+import com.querydsl.core.QueryResults;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.dto.QMemberTeamDto;
@@ -40,6 +43,77 @@ public class MemberRepositoryImpl implements MemberRepositoryCustom {
                         nullableExpression(member.age::loe, condition.getAgeLoe())
                 )
                 .fetch();
+    }
+
+    @Override
+    public Page<MemberTeamDto> searchPageSimple(MemberSearchCondition condition, Pageable pageable) {
+        QueryResults<MemberTeamDto> results = queryFactory
+                .select(new QMemberTeamDto(
+                        member.id.as("memberId"),
+                        member.username,
+                        member.age,
+                        team.id.as("teamId"),
+                        team.name.as("teamName")))
+                .from(member)
+                .join(member.team, team)
+                .where(
+                        nullableExpression(member.username::eq, condition.getUsername()),
+                        nullableExpression(member.team.name::eq, condition.getTeamName()),
+                        nullableExpression(member.age::goe, condition.getAgeGoe()),
+                        nullableExpression(member.age::loe, condition.getAgeLoe())
+                )
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetchResults();
+
+        List<MemberTeamDto> content = results.getResults();
+        long total = results.getTotal();
+
+        return new PageImpl<>(content, pageable, total);
+    }
+
+    @Override
+    public Page<MemberTeamDto> searchPageComplex(MemberSearchCondition condition, Pageable pageable) {
+        List<MemberTeamDto> content = queryContents(condition, pageable);
+        // 카운트 쿼리를 좀더 최적화 할 수 있는 경우 별도의 쿼리로 작성할 수 있다.
+        long total = queryCount(condition);
+
+        return new PageImpl<>(content, pageable, total);
+    }
+
+    private List<MemberTeamDto> queryContents(MemberSearchCondition condition, Pageable pageable) {
+        return queryFactory
+                .select(new QMemberTeamDto(
+                        member.id.as("memberId"),
+                        member.username,
+                        member.age,
+                        team.id.as("teamId"),
+                        team.name.as("teamName")))
+                .from(member)
+                .join(member.team, team)
+                .where(
+                        nullableExpression(member.username::eq, condition.getUsername()),
+                        nullableExpression(member.team.name::eq, condition.getTeamName()),
+                        nullableExpression(member.age::goe, condition.getAgeGoe()),
+                        nullableExpression(member.age::loe, condition.getAgeLoe())
+                )
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+    }
+
+    private long queryCount(MemberSearchCondition condition) {
+        return queryFactory
+                .select(member)
+                .from(member)
+                .join(member.team, team)
+                .where(
+                        nullableExpression(member.username::eq, condition.getUsername()),
+                        nullableExpression(member.team.name::eq, condition.getTeamName()),
+                        nullableExpression(member.age::goe, condition.getAgeGoe()),
+                        nullableExpression(member.age::loe, condition.getAgeLoe())
+                )
+                .fetchCount();
     }
 
     private BooleanExpression nullableExpression(Function<String, BooleanExpression> function, String value) {
